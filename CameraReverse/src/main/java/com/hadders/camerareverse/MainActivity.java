c449dfa0e9c2729884e0c626fb5062cdae4bdf86
Steven Hadley <steven.hadley@neonstingray.com>
2013-07-10 21:36:46 +1000

    remove redundant files

--- CameraReverse/src/main/java/com/hadders/camerareverse/MainActivity.java
+++ CameraReverse/src/main/java/com/hadders/camerareverse/MainActivity.java
@@ -1,177 +1,40 @@
 package com.hadders.camerareverse;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.TextureView;
-import android.view.View;
-
-import com.hadders.camerareverse.util.SystemUiHider;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
+ * @author Steven Hadley
+ *         <p/>
+ *         Shows an image preview NOT mirrored.
+ *         This is useful if you want to capture or scan images from a front-facing camera
  */
 public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-
     private Camera mCamera;
-
     private TextureView mTextureView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_screen);
-
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
         mTextureView = (TextureView) findViewById(R.id.camera_texture);
         mTextureView.setSurfaceTextureListener(this);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, mTextureView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mTextureView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+
         Camera.CameraInfo info = new Camera.CameraInfo();
         for (int i = 0; i < Camera.getNumberOfCameras(); i++) {
             Camera.getCameraInfo(i, info);
@@ -182,12 +45,24 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             }
         }
 
-        mCamera.setDisplayOrientation(90);
+        if (null == mCamera) {
+            Log.e("camera-reverse", "no front facing camera");
+            Toast toast = Toast.makeText(this.getApplicationContext(), "No front facing camera", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
 
-        // Front faced cameras mirror the image so we must convert it back in
-        // the x-plane
+        /*
+           This doesn't work
+           setCameraDisplayOrientation(info, mCamera);
+         */
+
+        //make portrait
+        mCamera.setDisplayOrientation(90);
+        //create a matrix to invert the x-plane
         Matrix matrix = new Matrix();
         matrix.setScale(-1, 1);
+        //move it back to in view otherwise it'll be off to the left.
         matrix.postTranslate(width, 0);
         mTextureView.setTransform(matrix);
 
@@ -196,7 +71,7 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             mCamera.setPreviewTexture(surfaceTexture);
             mCamera.startPreview();
         } catch (IOException ioe) {
-            Log.e("camera", ioe.getMessage());
+            Log.e("camera-reverse", ioe.getMessage());
         }
     }
 
@@ -218,9 +93,24 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
         //This is where you get the image to check for barcode
 
-        //Bitmap barcodeBmp = mTextureView.getBitmap();
+        // read the image from the SurfaceTexture
+        Bitmap barcodeBmp = mTextureView.getBitmap();
 
-        //Process
+        //get pixel array
+        int width = barcodeBmp.getWidth();
+        int height = barcodeBmp.getHeight();
+        int[] pixels = new int[barcodeBmp.getHeight() * barcodeBmp.getWidth()];
+        barcodeBmp.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        /*
+        If using zbar barcode processing library.
+
+        // create a barcode image
+		Image barcode = new Image(width, height, "RGB4");
+		barcode.setData(pixels);
+		int result = mScanner.scanImage(barcode.convert("Y800"));
+         */
+
     }
 
     public void onPause() {
@@ -234,5 +124,39 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    /**
+     * From http://developer.android.com/reference/android/hardware/Camera.html#setDisplayOrientation%28int%29
+     *
+     * @param info
+     * @param camera
+     */
+    public void setCameraDisplayOrientation(
+            Camera.CameraInfo info, android.hardware.Camera camera) {
+
+        int rotation = this.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        result = (info.orientation + degrees) % 360;
+        result = (360 - result) % 360;  // compensate the mirror
+
+        camera.setDisplayOrientation(result);
     }
 }
